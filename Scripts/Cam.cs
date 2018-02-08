86edc923ff43e0c6a7da3175ea6ed13bd6806772
AyoubProg <ayoub.aboata@hotmail.com>
2018-02-08 13:13:30 +0200

    Button events

---
+++ Scripts/Cam.cs
@@ -0,0 +1,68 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cam : MonoBehaviour
+{
+    private bool camAvailable;
+    private WebCamTexture backCam;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    // Use this for initialization
+    private void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("no camera detected");
+            camAvailable = false;
+        }
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (!devices[i].isFrontFacing)
+            {
+                backCam = new WebCamTexture(devices[i].name);
+            }
+
+        }
+        if (backCam == null)
+        {
+            Debug.Log("unable to find back camera");
+            return;
+        }
+        backCam.Play();
+        background.texture = backCam;
+
+        camAvailable = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!camAvailable)
+        {
+            return;
+        }
+        //give you a ratio that we'll be using in our aspect fitters- proper ratio 
+        float ratio = (float)backCam.width / (float)backCam.height;
+        fit.aspectRatio = ratio;
+
+        //proper scale
+        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        //proper angle
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+
+
+    }
+
+}
